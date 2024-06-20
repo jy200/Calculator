1884dd53292c50b64aa6428867443d64f01ec83b
Jamin Yang <jaminyang2000@gmail.com>
2024-06-19 23:27:23 -0400

    Css changes, Readme

---
+++ README.md
@@ -0,0 +1,10 @@
+Run
+- git clone
+- run index.html
+- Press buttons on calculator
+  - C: reset screen
+  - Del: delete one number
+  - Addition, Subtraction, Multiplication, Division
+  - Does not support addition of negative numbers (although subtraction does work)
+
+![Demo](demo.webp)
--- main.js
+++ main.js
@@ -3,79 +3,77 @@ let number = screenNumber.innerText;
 console.log(screenNumber);
 
 const calc = document.querySelector("div");
-// console.log(calc.innerHTML);
-const single = ['/', '*', '-', '+', '.']
-calc.addEventListener('click', event => {
-    const maybeButton = event.target;
-    const isButton = maybeButton.nodeName === 'BUTTON';
-    if (!isButton) {
-      return;
-    }else{
-        const clicked = maybeButton.innerText;
-        if (clicked === "="){
-            const lastChar = number.substring(number.length - 1);
-            if (!single.includes(lastChar)){
-                calculate(number);
-            }
-        }else if (clicked === "C"){
-            updateScreen(0);
-        }else if (clicked === "DEL"){
-            if(number.length > 0 && number != 0){
-                number = number.substring(0, number.length - 1);
-                updateScreen(number);
-            }
-        }else if (single.includes(clicked)){
-            const lastChar = number.substring(number.length - 1);
-            if (single.includes(lastChar)){
-                return;
-            }else{
-                updateScreen(number+clicked);
-            }  
-        }else{
-            const lastChar = number.substring(number.length - 1);
-            if (lastChar == "0"){
-                updateScreen(clicked);
-            }else{
-                updateScreen(number+clicked);
-            }
-        }
+const single = ["/", "*", "-", "+", "."];
+calc.addEventListener("click", (event) => {
+  const maybeButton = event.target;
+  const isButton = maybeButton.nodeName === "BUTTON";
+  if (!isButton) {
+    return;
+  } else {
+    const clicked = maybeButton.innerText;
+    if (clicked === "=") {
+      const lastChar = number.substring(number.length - 1);
+      if (!single.includes(lastChar)) {
+        calculate(number);
+      }
+    } else if (clicked === "C") {
+      updateScreen(0);
+    } else if (clicked === "DEL") {
+      if (number.length > 0 && number != 0) {
+        number = number.substring(0, number.length - 1);
+        updateScreen(number);
+      }
+    } else if (single.includes(clicked)) {
+      const lastChar = number.substring(number.length - 1);
+      if (single.includes(lastChar)) {
+        return;
+      } else {
+        updateScreen(number + clicked);
+      }
+    } else {
+      const lastChar = number.substring(number.length - 1);
+      if (lastChar == "0") {
+        updateScreen(clicked);
+      } else {
+        updateScreen(number + clicked);
+      }
     }
-})
+  }
+});
 
-function updateScreen(newNumber){
-    screenNumber.textContent  = newNumber;
-    number = screenNumber.innerText;
+function updateScreen(newNumber) {
+  screenNumber.textContent = newNumber;
+  number = screenNumber.innerText;
 }
-const operations = ['/', '*', '-', '+'] 
-function calculate(newNumber){
-    let result = null;
-    let screenSplit = newNumber.split(/(\d*\.*\d*)/).filter(Boolean);
-    console.log(screenSplit);
-    let lastOperation = null;
-    let first_iteration = true;
-    for (i of screenSplit){
-        console.log('i: '+i);
-        if (operations.includes(i)){
-            lastOperation = i
-            first_iteration = false;
-        }else{
-            if (first_iteration){
-                result = parseFloat(i);
-                first_iteration = false;
-            }else{
-                console.log(lastOperation);
-                if (lastOperation == '/'){
-                    result = result / parseFloat(i);
-                } else if (lastOperation == '*'){
-                    result = result * parseFloat(i);
-                } else if (lastOperation == '+'){
-                    result = result + parseFloat(i);
-                } else if (lastOperation == '-'){
-                    result = result - parseFloat(i);
-                }
-            }
+const operations = ["/", "*", "-", "+"];
+function calculate(newNumber) {
+  let result = null;
+  let screenSplit = newNumber.split(/(\d*\.*\d*)/).filter(Boolean);
+  console.log(screenSplit);
+  let lastOperation = null;
+  let first_iteration = true;
+  for (i of screenSplit) {
+    console.log(i);
+    if (operations.includes(i)) {
+      lastOperation = i;
+      first_iteration = false;
+    } else {
+      if (first_iteration) {
+        result = parseFloat(i);
+        first_iteration = false;
+      } else {
+        // console.log(lastOperation);
+        if (lastOperation == "/") {
+          result = result / parseFloat(i);
+        } else if (lastOperation == "*") {
+          result = result * parseFloat(i);
+        } else if (lastOperation == "+") {
+          result = result + parseFloat(i);
+        } else if (lastOperation == "-") {
+          result = result - parseFloat(i);
         }
-        // console.log(result);
+      }
     }
-    updateScreen(result);
+  }
+  updateScreen(result);
 }
